582618406017dc57d8e6d9c32d9c76894002ee70
shubham-nov06 <dograshubham06@gmail.com>
2025-11-18 20:14:23 +0530

    Added file 1.py

---
+++ 1.py
@@ -0,0 +1,15 @@
+name = "My name is Shubham "
+print(name)
+print(name + "Dogra")
+
+age = 18 
+print(age)
+
+print("Enter your age ")
+age1 = int(input())
+print("Your age is " , age1)
+
+print("Enter your name  ")
+
+name1 = str(input())
+print("Your name is " , name1)

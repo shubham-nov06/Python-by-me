f9f0a2ab2d8ed1763750051eaf2ed66d3476ccb2
shubham-nov06 <dograshubham06@gmail.com>
2025-11-20 22:03:28 +0530

    Updated changes

--- 3.py
+++ 3.py
@@ -1,5 +1,4 @@
-
-
+# A sinmple project to change Interactive String Manipulation Program 
 name = str(input("Enter your name "))
 print("Your name is = " , name)
 print("What do you want to do eg- isalpha , isalnum , lower , upper , title , isupper ")

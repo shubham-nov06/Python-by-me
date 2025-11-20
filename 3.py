f0d774130da0256eb8797a70ddf9145845de42d1
shubham-nov06 <dograshubham06@gmail.com>
2025-11-20 21:49:39 +0530

    Updated changes

--- 3.py
+++ 3.py
@@ -2,4 +2,4 @@ name = str(input("Enter your name "))
 print("Your name is = " , name )
 print("What do you want to do eg- isalpha , isalnum , lower , upper , title , isupper ")
 
-input("Enter it = ")
+operation = input("Enter it = ")

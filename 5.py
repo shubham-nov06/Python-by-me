5c283145b17ab15312b58083ed4000a5030d52c1
shubham-nov06 <dograshubham06@gmail.com>
2025-11-21 12:32:34 +0530

    Updated changes

--- 5.py
+++ 5.py
@@ -7,3 +7,7 @@ ch = int(input("Choose the option"))
 if ch== 1 :
     c = float(input("Celcius"))
     print("Fahrenheit = " , (c *9/5) + 32)
+    
+elif ch==2:
+    f = input("Fahrenheit")
+    print("Fahrenheit = " , (f - 32) * 5/7)

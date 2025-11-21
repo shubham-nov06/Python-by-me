30cbf3c382d816d951d21a65a945db34d4e12846
shubham-nov06 <dograshubham06@gmail.com>
2025-11-21 12:34:20 +0530

    Updated changes

--- 5.py
+++ 5.py
@@ -4,10 +4,10 @@ print(" 2 - Fahrenheit to Celsius ")
 
 ch = int(input("Choose the option"))
 
-if ch== 1 :
+if ch == 1:
     c = float(input("Celcius"))
-    print("Fahrenheit = " , (c *9/5) + 32)
+    print("Fahrenheit = " , (c * 9 / 5) + 32)
     
-elif ch==2:
+elif ch == 2:
     f = float(input("Fahrenheit"))
-    print("Celcius = " , (f - 32) * 5/9)
+    print("Celcius = " , (f - 32) * 5 / 9)

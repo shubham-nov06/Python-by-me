a9cb48af76d79bd7515ddb93a70896523b4a8c14
shubham-nov06 <dograshubham06@gmail.com>
2025-11-22 15:10:43 +0530

    Updated changes

--- 3.py
+++ 3.py
@@ -7,6 +7,7 @@ print("Your name is = " , name)
 print("What do you want to do eg- isalpha , isalnum , lower , upper , title , isupper ")
 
 operation = input("Enter it = ")
+
 if operation == 'lower': 
     print(name.lower())
     

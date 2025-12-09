a6dbda6f9c4db22498def70e3f7c753c055bb9d0
shubham-nov06 <dograshubham06@gmail.com>
2025-12-09 14:09:26 +0000

    Updated changes

--- 9.py
+++ 9.py
@@ -4,6 +4,10 @@ side3 = int(input("Enter 3rd side of triangle = "))
 
 if side1 == side2 and side2 == side3:
     ("This is a Eqilateral Triangle ")
+    
 elif side1 == side2 or side2 == side3 or side1 == side3:
+    
     print("This is a Isocles Traiangle")
-else: print("This is scalene Triangle ")
+    
+else: 
+    print("This is scalene Triangle ")

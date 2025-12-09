9aea511f0a46dd707ffcc26f1ed63147c27062d4
shubham-nov06 <dograshubham06@gmail.com>
2025-12-09 14:09:26 +0000

    Updated changes

--- 9.py
+++ 9.py
@@ -3,7 +3,8 @@ side2 = int(input("Enter 2nd side of triangale = "))
 side3 = int(input("Enter 3rd side of triangle = "))
 
 if side1 == side2 and side2 == side3:
-    ("This is a Eqilateral Triangle ")
+    
+   print("This is a Eqilateral Triangle ")
     
 elif side1 == side2 or side2 == side3 or side1 == side3:
     

1bd267a66ee1584ba8c029d4162295fe8066e64e
shubham-nov06 <dograshubham06@gmail.com>
2025-12-09 14:13:37 +0000

    Updated changes

--- 10.py
+++ 10.py
@@ -1,3 +1,12 @@
 time = float(input("Enter time in hours "))
 
-
+if time >= 5 and time<=11:
+    print("Morning")
+elif time > 12 and time <=16:
+    print("Good afternoon ")
+    
+elif time > 17 and time <= 20:
+    print("Good evening ")
+    
+elif time > 21 and time == 4 :
+    print("Night ")      
